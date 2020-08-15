f3a2f047db3dae808f9e3eec499fb59bd439df58
oonray <alexanderbjornsrud@gmail.com>
2020-08-15 14:38:30 +0200

    .

--- decryptor.h
+++ decryptor.h
@@ -1,11 +1,22 @@
+#include <Windows.h>
+//-----------------------
+#include <wincrypt.h>
+#pragma comment(lib, "crypt32.lib")
+
 #include <iostream>
+#define AES_KEY_SIZE 16
+#define CHUNK_SIZE (AES_KEY_SIZE * 3)
 
 namespace DEC {
-class Decryptor{
+class Decryptor {
 private:
     std::string cleartext;
+    std::string password;
+
 public:
-    Decryptor();
+    Decryptor(std::string pw, std::string cl);
     ~Decryptor();
+    std::string decrypt();
 };
-}
+}  // namespace DEC
+
--- encryptor.h
+++ encryptor.h
@@ -1,12 +1,29 @@
-#include "strings.h"
+#include <Windows.h>
+//-------------------------
+#include <wincrypt.h>
+#pragma comment(lib, "crypt32.lib")
+
 #include <iostream>
 
+#include "strings.h"
+
+#ifndef __encryptor_h
+#define __encryptor_h
+
+#define AES_KEY_SIZE 16
+#define CHUNK_SIZE (AES_KEY_SIZE * 3)
+
 namespace ENC {
-class Encryptor{
+class Encryptor {
 private:
     std::string cleartext;
+    std::string password;
+
 public:
-    Encryptor();
+    Encryptor(std::string pw, std::string cl);
     ~Encryptor();
+    std::string encrypt();
 };
-}
+}  // namespace ENC
+
+#endif
